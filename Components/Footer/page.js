5a8c9d925cdbbc3594b147e781b0eab04d004d92
bantu-art <brianbaraza40@.com>
2024-12-13 15:29:56 +0300

    feat: add SavePulse footer and navbar sections

--- Components/Footer/page.js
+++ Components/Footer/page.js
@@ -2,13 +2,13 @@ import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <div className="footerdiv">
-      <p className="copyright">© 2024</p>
-      <ul className="socialLinks">
-        <li className="socialLinkItem">Twitter</li>
-        <li className="socialLinkItem">Facebook</li>
-        <li className="socialLinkItem">LinkedIn</li>
-        <li className="socialLinkItem">Instagram</li>
+    <div className={styles.footerdiv}>
+      <p className={styles.copyright}>© 2024 LifeShare. Saving Lives Together.</p>
+      <ul className={styles.socialLinks}>
+        <li className={styles.socialLinkItem}>Twitter</li>
+        <li className={styles.socialLinkItem}>Facebook</li>
+        <li className={styles.socialLinkItem}>LinkedIn</li>
+        <li className={styles.socialLinkItem}>Instagram</li>
       </ul>
     </div>
   );
