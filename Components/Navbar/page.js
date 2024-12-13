ac616313deb63eeeac86b1177e42249d1275f31f
bantu-art <brianbaraza40@.com>
2024-12-13 19:04:12 +0300

    feat: style navbar to reflect on the signup page

--- Components/Navbar/page.js
+++ Components/Navbar/page.js
@@ -3,11 +3,9 @@ import styles from "./navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className={`${styles.nav} flex items-center justify-between`}>
-      <div className="flex items-center">
-        <h1 className="text-2xl font-bold text-red-600 mr-6">SavePulse</h1>
-      </div>
-      <div className="flex space-x-4">
+    <nav className={styles.nav}>
+      <div className={styles["nav-logo"]}>SavePulse</div>
+      <div className={styles["nav-links"]}>
         <Link href="/" className={styles["nav-link"]}>Home</Link>
         <Link href="/about" className={styles["nav-link"]}>About Us</Link>
         <Link href="/contact" className={styles["nav-link"]}>Contact</Link>
