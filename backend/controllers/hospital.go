6a8a8b6da1be0fddece9b31114212cd3520c8e6c
Cliffdoyle <oyoocliff471@gmail.com>
2024-12-13 23:59:08 +0300

    Add function that retrieves hospital requests

---
+++ backend/controllers/hospital.go
@@ -0,0 +1,12 @@
+// GetHospitalRequests retrieves blood requests made by a hospital.
+func GetHospitalRequests(c *gin.Context) {
+	hospitalID := c.Param("hospitalID")
+
+	var requestsCount int64
+	if err := initializers.DB.Model(&models.Donation).Where("current_stage = ? AND facility_name = ?", "Hospital", hospitalID).Count(&requestsCount).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve requests"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"totalRequests": requestsCount})
+}
