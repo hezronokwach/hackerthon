5595970152aacb499db0a35b0f82d32539a89704
bantu-art <brianbaraza40@.com>
2024-12-15 01:14:58 +0300

    feat: implement login dform and its style

--- app/login/page.js
+++ app/login/page.js
@@ -37,7 +37,7 @@ export default function Login() {
 
     return (
         <div className={styles.container}>
-            <h1 className={styles.pageTitle}>Login Page</h1>
+            <h1 className={styles.pageTitle}>Donor Login</h1>
             <form onSubmit={handleSubmit} className={styles.form}>
                 <div className={styles.formGroup}>
                     <label htmlFor="email">Email</label>
