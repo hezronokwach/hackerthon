78ff9e4168c6a3f6f09caaaf6e2ff93501cb7619
akothouma <lornaakothpb@gmail.com>
2024-12-14 16:29:00 +0300

    Add:Regional  path

---
+++ app/Regional/add/page.js
@@ -0,0 +1,5 @@
+export default function HospitalData(){
+    return(
+        <h1>This is Regional add entry page</h1>
+    )
+}
---
+++ app/Regional/approve/page.js
@@ -0,0 +1,5 @@
+export default function HospitalData(){
+    return(
+        <h1>This is all of the requests</h1>
+    )
+}
