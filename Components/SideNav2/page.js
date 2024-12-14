03b1c9c487fe23228a7ffa1a43b1ad0d090e9d5a
akothouma <lornaakothpb@gmail.com>
2024-12-14 18:35:45 +0300

    Add:all dashboards

---
+++ Components/SideNav2/page.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+
+export default function SideNav2() {
+    return (
+        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+            <Link href="/Regional/add" className="nav-link">Request Blood</Link>
+            <Link href="/Regional/update" className="nav-link">Update Blood</Link>
+            <Link href="/Regional/approve" className="nav-link">Blood Requests</Link>
+            
+
+            <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+
+                <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+                <form>
+                    <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+
+                        <div className="hidden md:block">Sign Out</div>
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
