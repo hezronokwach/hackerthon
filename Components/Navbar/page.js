bd21d13c90b6f5c5eef79a8f9b5f1d30dc69851d
bantu-art <brianbaraza40@.com>
2024-12-15 01:20:12 +0300

    feat: implement changes to the navbar and footer

--- Components/Navbar/page.js
+++ Components/Navbar/page.js
@@ -1,13 +1,18 @@
-
 import Link from "next/link";
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <Link href="/" className="nav-link">Home</Link>
-      <Link href="/about" className="nav-link">About Us</Link>
-      <Link href="/contact" className="nav-link">Contact</Link>
-      <Link href="/FAQs" className="nav-link">FAQs</Link>
+    <nav className={styles.nav}>
+      <div className={styles["nav-logo"]}>
+        <Link href="/" className={styles["nav-link"]}>SavePulse</Link>
+      </div>
+      <div className={styles["nav-links"]}>
+        <Link href="/" className={styles["nav-link"]}>Home</Link>
+        <Link href="/about" className={styles["nav-link"]}>About Us</Link>
+        <Link href="/contact" className={styles["nav-link"]}>Contact</Link>
+        <Link href="/FAQs" className={styles["nav-link"]}>FAQs</Link>
+      </div>
     </nav>
   );
 }
